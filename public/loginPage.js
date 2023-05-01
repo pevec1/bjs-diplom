6526f503feb41e71f69e1a8a324f1016629001da
pevec1 <pevec1@yandex.ru>
2023-05-01 15:46:04 +0300

    update login and register

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,27 +1,27 @@
 "use strict";
 let user = new UserForm();
-let data = {};
+
 //let data = {login: "vladimir@demo.ru", password: "demo"}
 user.loginFormCallback = function(data){
-    ApiConnector.login(data, (response, data) => 
+    ApiConnector.login(data, (response) => 
     {
         if (response.success === false) {
             user.setLoginErrorMessage(response.error);
         } else{
-            console.log(response);
-            setTimeout(() => location.reload(), 1000);
+           // console.log(response);
+            location.reload();
         }
     })
 }
 
 user.registerFormCallback = function(data){
-    ApiConnector.register(data, (response, data) => 
+    ApiConnector.register(data, (response) => 
     {
         if (response.success === false) {
             user.setRegisterErrorMessage(response.error);
         } else {
 
-            console.log(response);
+            //console.log(response);
             location.reload()
         }
     });
